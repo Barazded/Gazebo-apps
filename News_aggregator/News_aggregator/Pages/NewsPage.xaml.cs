a8649837f4c238271034a9fc1d9538b44fe4c26d
Barazded <92597611+Barazded@users.noreply.github.com>
2023-02-01 19:27:21 +0300

    V1.4.1
    update

--- News_aggregator/News_aggregator/Pages/NewsPage.xaml.cs
+++ News_aggregator/News_aggregator/Pages/NewsPage.xaml.cs
@@ -1,18 +1,16 @@
 ﻿using Parser.Core;
 using Parser.Core.Habra;
-using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
+using News_aggregator.Parser;
 
 namespace News_aggregator.Pages
 {
     public partial class NewsPage : ContentPage
     {
         readonly ParserWorker<string[]> parser;
-        //G - global
-        //public List<string> GTitles = new List<string>();
-        //public List<string> GInfo = new List<string>();
-        //public List<string> GDates = new List<string>();
+        private List<Card> resoursesList = new List<Card>();
+
         public NewsPage()
         {
             InitializeComponent();
@@ -20,26 +18,31 @@ namespace News_aggregator.Pages
             parser = new ParserWorker<string[]>(new IgromaniaParser());
             parser.OnNewData += Parser_OnNewData;
             //нужно создать отдельный класс карточки и передавать список экземпляров этого класса в ItemsSource
-            //свойства id, title, date, info
+            collectionResourses.ItemsSource = resoursesList;
         }
-        protected async override void OnAppearing()
+        protected override void OnAppearing()
         {
-            collectionResourses.ItemsSource = await App.DataBase.GetItemsAsync();
             parser.Settings = new IgromaniaSettings(0, 5);
             parser.Start();
         }
-        private void Parser_OnNewData(object sender, List<string> titles, List<string> info, List<string> dates)
+        private async void Parser_OnNewData(object sender, List<string> titles, List<string> info, List<string> dates)
         {
             //урезание массива под нужный размер(мда)
             var endPoint = parser.Settings.EndPoint;
             titles.RemoveRange(5, titles.Count - 5);
             info.RemoveRange(5, info.Count - 5);
             dates.RemoveRange(5, dates.Count - 5);
-            //
-            //GTitles = titles;
-            //await DisplayAlert("print", $"{GTitles[0]}", "ok");
-            //GInfo = info;
-            //GDates = dates;
+
+            for (int i = 0; i < 5; i++)
+            {
+                var newCard = new Card();
+                newCard.Title = titles[i];
+                newCard.Info = info[i];
+                newCard.Date = dates[i];
+
+                resoursesList.Add(newCard);
+            }
+            await DisplayAlert("", $"{resoursesList.Count}", "ok");
         }
     }
 }
---
+++ News_aggregator/News_aggregator/Parser/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace News_aggregator.Parser
+{
+	public class Card
+	{
+        public string Title { get; set; }
+
+        public string Info { get; set; }
+
+        public string Date { get; set; }
+    }
+}
+
