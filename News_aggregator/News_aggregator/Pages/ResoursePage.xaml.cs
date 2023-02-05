bf688af958f4c1401dd3b8b04b15986a02acb84c
Barazded <92597611+Barazded@users.noreply.github.com>
2023-02-05 23:02:38 +0300

    Revert "V1.4"
    This reverts commit 697643c555a65106157bfca488e88b4ab3daa564.

--- News_aggregator/News_aggregator/Pages/ResoursePage.xaml.cs
+++ News_aggregator/News_aggregator/Pages/ResoursePage.xaml.cs
@@ -8,9 +8,9 @@ namespace News_aggregator.Pages
     public partial class ResoursePage : ContentPage
     {
         //для поиска по бд
-        private List<ResourseItem> resourses = new List<ResourseItem>();
+        public List<ResourseItem> resourses = new List<ResourseItem>();
         //для сохранение в бд
-        private List<ResourseItem> resoursesForSave = new List<ResourseItem>();
+        public List<ResourseItem> resoursesForSave = new List<ResourseItem>();
         public ResoursePage()
         {
             InitializeComponent();
--- News_aggregator/News_aggregator/Parser/IParserSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Parser_html.Core
-{
-    internal interface IParserSettings
-    {
-        string BaseUrl { get; set; }
-        int StartPoint { get; set; }
-        int EndPoint { get; set; }
-    }
-}
