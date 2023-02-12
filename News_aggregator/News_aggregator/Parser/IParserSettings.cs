8a58aabdf768c7cb2ddd5ce1dbc2c867434d9a80
Barazded <92597611+Barazded@users.noreply.github.com>
2023-02-12 15:51:57 +0300

    Delete

--- News_aggregator/News_aggregator/Parser/IParserSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace News_aggregator.Parser
-{
-    internal interface IParserSettings
-    {
-        string BaseUrl { get; set; }
-        int StartPoint { get; set; }
-        int EndPoint { get; set; }
-    }
-}
